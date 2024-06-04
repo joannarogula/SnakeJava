741850f56800fd76e610a51883d8464490cf4825
Joanna <263536@student.pwr.edu.pl>
2024-06-04 15:40:52 +0200

    wall-collision

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,7 +12,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int WIDTH = 500;
     static final int HEIGHT = 500;
     static final int UNIT_SIZE = 10;
-    static final int ELEMENTS = 5;
+    static final int ELEMENTS = 3;
     static final int OBSTACLES_NUM = 8;
     static final int GAME_UNITS = (WIDTH * HEIGHT) / UNIT_SIZE;
     static final int DELAY = 60;
@@ -48,8 +48,13 @@ public class GamePanel extends JPanel implements ActionListener {
     int xDirection = UNIT_SIZE;
     int yDirection = 0;
 
+    private boolean gameStarted = false;
+
     Action turnUpAction = new AbstractAction() {
         public void actionPerformed(ActionEvent e) {
+            if (!gameStarted) {
+                gameStarted = true;
+            }
             if (direction != Direction.DOWN) {
                 direction = Direction.UP;
                 xDirection = 0;
@@ -60,6 +65,9 @@ public class GamePanel extends JPanel implements ActionListener {
 
     Action turnDownAction = new AbstractAction() {
         public void actionPerformed(ActionEvent e) {
+            if (!gameStarted) {
+                gameStarted = true;
+            }
             if (direction != Direction.UP) {
                 direction = Direction.DOWN;
                 xDirection = 0;
@@ -70,6 +78,9 @@ public class GamePanel extends JPanel implements ActionListener {
 
     Action turnLeftAction = new AbstractAction() {
         public void actionPerformed(ActionEvent e) {
+            if (!gameStarted) {
+                gameStarted = true;
+            }
             if (direction != Direction.RIGHT) {
                 direction = Direction.LEFT;
                 xDirection = -UNIT_SIZE;
@@ -80,6 +91,9 @@ public class GamePanel extends JPanel implements ActionListener {
 
     Action turnRightAction = new AbstractAction() {
         public void actionPerformed(ActionEvent e) {
+            if (!gameStarted) {
+                gameStarted = true;
+            }
             if (direction != Direction.LEFT) {
                 direction = Direction.RIGHT;
                 xDirection = UNIT_SIZE;
@@ -87,6 +101,26 @@ public class GamePanel extends JPanel implements ActionListener {
             }
         }
     };
+
+    private void initializePlayerSnake() {
+        // Ustawienie losowej pozycji głowy węża w bezpiecznym zakresie
+        xCoord[0] = random.nextInt((WIDTH / UNIT_SIZE) - 2) * UNIT_SIZE + UNIT_SIZE;
+        yCoord[0] = random.nextInt((HEIGHT / UNIT_SIZE) - 2) * UNIT_SIZE + UNIT_SIZE;
+
+        // Ustawienie segmentów ciała węża za głową
+        for (int i = 1; i < segments; i++) {
+            xCoord[i] = xCoord[0] - i * UNIT_SIZE;
+            yCoord[i] = yCoord[0];
+        }
+    }
+
+    private void placeFruits() {
+        for (int i = 0; i < ELEMENTS; i++) {
+            // Losowo generujemy pozycje dla jabłek
+            xFruits[i] = random.nextInt((WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+            yFruits[i] = random.nextInt((HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+        }
+    }
 
     private void move() {
         for (int i = segments; i > 0; i--) {
@@ -98,9 +132,49 @@ public class GamePanel extends JPanel implements ActionListener {
         yCoord[0] += yDirection;
     }
 
+    private void checkFruit() {
+      System.out.println("CheckFruit");
+        for (int i = 0; i < ELEMENTS; i++) {
+            if (xCoord[0] == xFruits[i] && yCoord[0] == yFruits[i]) {
+                // Wąż zjadł jabłko, więc zwiększamy długość węża
+                segments++;
+                // Generujemy nowe położenie jabłka
+                xFruits[i] = random.nextInt((WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+                yFruits[i] = random.nextInt((HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+                // Zwiększamy wynik (lub robimy coś innego)
+                // increaseScore();
+            }
+        }
+    }
 
+    private void checkCollisions() {
+        // Sprawdzanie kolizji z krawędziami planszy
+        if (xCoord[0] < 0 || xCoord[0] >= WIDTH || yCoord[0] < 0 || yCoord[0] >= HEIGHT) {
+            isRunning = false;
+        }
 
-GamePanel() {
+        // Sprawdzanie kolizji z samym sobą
+        for (int i = segments; i > 0; i--) {
+            if ((xCoord[0] == xCoord[i]) && (yCoord[0] == yCoord[i])) {
+                isRunning = false;
+            }
+        }
+
+        // Sprawdzanie kolizji z przeszkodami
+        // for (int i = 0; i < obstaclesX.length; i++) {
+        //     for (int j = 0; j < obstaclesX[i].length; j++) {
+        //         if (xCoord[0] == obstaclesX[i][j] && yCoord[0] == obstaclesY[i][j]) {
+        //             isRunning = false;
+        //         }
+        //     }
+        // }
+        // Zatrzymanie timera, jeśli gra się skończyła
+        if (!isRunning) {
+            timer.stop();
+        }
+    }
+
+    GamePanel() {
         random = new Random();
         record = 0;  // Ustawienie domyślnej wartości rekordu na 0
 
@@ -133,6 +207,9 @@ GamePanel() {
         this.getActionMap().put("turnLeft", turnLeftAction);
         this.getActionMap().put("turnRight", turnRightAction);
 
+        initializePlayerSnake();
+        placeFruits();
+
         for (int i = 0; i < aiSegments; i++) {
             aiXCoord[i] = WIDTH;
             aiYCoord[i] = HEIGHT;
@@ -157,18 +234,21 @@ GamePanel() {
     //     // Kod, który ma się wykonać w odpowiedzi na akcję
     // }
     public void actionPerformed(ActionEvent e) {
-    if (isRunning) {
-        move();
-        // checkCollisions();
-        // checkFruit();
+        if (!gameStarted) {
+            return; // Nie wykonuj ruchu, jeśli gra się jeszcze nie rozpoczęła
+        }
+        if (isRunning) {
+            move();
+            checkCollisions();
+            checkFruit();
+        }
+        repaint();
     }
-    repaint();
-}
 
     @Override
-protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         if (isRunning) {
             // Rysowanie owoców
             g.setColor(Color.RED);
@@ -208,153 +288,3 @@ protected void paintComponent(Graphics g) {
         g.drawString(message, (WIDTH - metrics.stringWidth(message)) / 2, HEIGHT / 2);
     }
 };
-
-// import java.awt.*;
-// import java.awt.event.*;
-// import java.io.File;
-// import java.io.FileNotFoundException;
-// import java.util.Random;
-// import java.util.Scanner;
-// import javax.swing.*;
-
-// /**
-//  * @class GamePanel
-//  * Plansza do gry
-//  */
-
-// public class GamePanel extends JPanel implements ActionListener 
-// {
-//   static final int WIDTH = 500;
-//   static final int HEIGHT = 500;
-//   static final int UNIT_SIZE = 10;
-//   static final int ELEMENTS = 5;
-//   static final int OBSTACLES_NUM = 8;
-//   static final int GAME_UNITS = (WIDTH * HEIGHT) / UNIT_SIZE;
-//   static final int DELAY = 60;
-//   static final String DATAFILE = "record.dat";
-
-//   final int xCoord[] = new int[GAME_UNITS];
-//   final int yCoord[] = new int[GAME_UNITS];
-//   final int aiXCoord[] = new int[GAME_UNITS];
-//   final int aiYCoord[] = new int[GAME_UNITS];
-
-//   final int xFruits[] = new int[ELEMENTS];
-//   final int yFruits[] = new int[ELEMENTS];
-
-//   int segments = 6;
-//   int aiSegments = 6;
-//   int eatenFruits;
-//   int aiEatenFruits;
-//   int record;
-//   int xFrog;
-//   int yFrog;
-//   int[][] obstaclesX;
-//   int[][] obstaclesY;
-//   boolean isRunning = false;
-//   boolean gamerWon = false;
-//   boolean aiWon = false;
-//   Timer timer;
-//   Random random;
-//   Direction direction = Direction.RIGHT;
-//   Direction aiDirection = Direction.LEFT;
-//   Direction frogDirection = Direction.LEFT;
-//   File datafile;
-
-//   Action turnUpAction = new AbstractAction() {
-//     public void actionPerformed(ActionEvent e) {
-//       if (direction != Direction.DOWN) {
-//         direction = Direction.UP;
-//       }
-//     }
-//   };
-
-//   Action turnDownAction = new AbstractAction() {
-//     public void actionPerformed(ActionEvent e) {
-//       if (direction != Direction.UP) {
-//         direction = Direction.DOWN;
-//       }
-//     }
-//   };
-
-//   Action turnLeftAction = new AbstractAction() {
-//     public void actionPerformed(ActionEvent e) {
-//       if (direction != Direction.RIGHT) {
-//         direction = Direction.LEFT;
-//       }
-//     }
-//   };
-
-//   Action turnRightAction = new AbstractAction() {
-//     public void actionPerformed(ActionEvent e) {
-//       if (direction != Direction.LEFT) {
-//         direction = Direction.RIGHT;
-//       }
-//     }
-//   };
-
-//   /**
-//   * @brief konstruktor - inicjalizuje obsługę klawiszy oraz współrzędne węża AI
-//   * Ustawia początkowe współrzędne owoców
-//   * Wczytuje z pliku informację o najlepszym wyniku
-//   * Wywołuje metodę start()
-//   */
-
-//   GamePanel() 
-//   {
-//     random = new Random();
-
-//     Scanner istream = null;
-//     datafile = new File(DATAFILE);
-    
-//     try {
-//       istream = new Scanner(datafile);
-//     } catch (FileNotFoundException e) {
-//       System.out.println("Cannot open file");
-//     }
-
-//     // record = istream.nextInt();
-
-//     this.setSize(WIDTH, HEIGHT);
-//     this.setLocation(100, 0);
-//     this.setBackground(Color.DARK_GRAY);
-//     this.setFocusable(true);
-//     this.requestFocusInWindow(true);
-
-//     this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "turnUp");
-//     this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "turnDown");
-//     this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "turnLeft");
-//     this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "turnRight");
-
-//     this.getActionMap().put("turnUp", turnUpAction);
-//     this.getActionMap().put("turnDown", turnDownAction);
-//     this.getActionMap().put("turnLeft", turnLeftAction);
-//     this.getActionMap().put("turnRight", turnRightAction);
-  
-//     for (int i = 0; i < aiSegments; i++) {
-//       aiXCoord[i] = WIDTH;
-//       aiYCoord[i] = HEIGHT;
-//     }
-
-//     for (int i = 0; i < ELEMENTS; i++) {
-//       xFruits[i] = -1;
-//       yFruits[i] = -1;
-//     }
-
-//     start();
-//   }
-
-//   /**
-//    * @brief Ustawia początkowe położenie obiektów na planszy
-//    * Inicjalizuje timer, na którym oparte jest działanie programu
-//    */
-
-//   public void start() {
-//     isRunning = true;
-//     timer = new Timer(DELAY, this);
-//     timer.start();
-//   }
-
-//   public void actionPerformed(ActionEvent e) {
-//         // Kod, który ma się wykonać w odpowiedzi na akcję
-//     }
-// }
